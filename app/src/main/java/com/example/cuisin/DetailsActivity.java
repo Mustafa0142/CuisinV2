b4d8d8e3e7f76c3d6e80cfdc48e2ca864eb86e66
Mustafa0142 <mustafa.ciftci@student.ucll.be>
2022-11-28 13:30:11 +0100

    Updated details page. More details coming soon.

--- app/src/main/java/com/example/cuisin/DetailsActivity.java
+++ app/src/main/java/com/example/cuisin/DetailsActivity.java
@@ -6,24 +6,37 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import com.example.cuisin.Api.RecipeInformation;
 import com.example.cuisin.Api.ResultsList;
+import com.squareup.picasso.Picasso;
 
 public class DetailsActivity extends AppCompatActivity {
-    TextView recipe_instructions;
+    TextView recipe_title, recipe_source, recipe_likes,
+             recipe_servings, recipe_score, recipe_prep;
     Button recipe_button;
+    ImageView recipe_image;
+
     ApiCaller apiCaller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+//      om bar te verbergen van boven
+        this.getSupportActionBar().hide();
 
         int recipeId = Integer.parseInt(getIntent().getStringExtra("recipeId"));
 
-        recipe_instructions = findViewById(R.id.recipeInstructions);
+        recipe_title = findViewById(R.id.recipeTitle);
+        recipe_source = findViewById(R.id.recipeSource);
+        recipe_likes = findViewById(R.id.recipeLikes);
+        recipe_servings = findViewById(R.id.recipeServings);
+        recipe_score = findViewById(R.id.recipeScore);
+        recipe_prep = findViewById(R.id.recipePrep);
         recipe_button = findViewById(R.id.recipeButton);
+        recipe_image = findViewById(R.id.recipeImage);
 
         apiCaller = new ApiCaller(this);
         apiCaller.getRecipeInformation(detailsListener, recipeId);
@@ -32,7 +45,13 @@ public class DetailsActivity extends AppCompatActivity {
     private final ApiListener detailsListener = new ApiListener() {
         @Override
         public void getResponse(RecipeInformation recipeInformation) {
-            recipe_instructions.setText(recipeInformation.instructions);
+            recipe_title.setText(recipeInformation.title);
+            recipe_source.setText(recipeInformation.sourceName);
+            recipe_likes.setText(recipeInformation.aggregateLikes + " likes");
+            recipe_servings.setText(recipeInformation.servings + " servings!");
+            recipe_score.setText("Score: " + recipeInformation.spoonacularScore + "/100");
+            recipe_prep.setText("Ready in " + recipeInformation.readyInMinutes + " minutes!");
+            Picasso.get().load(recipeInformation.image).into(recipe_image);
 
             recipe_button.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -50,8 +69,6 @@ public class DetailsActivity extends AppCompatActivity {
         }
 
         @Override
-        public void getResponse(ResultsList resultsList) {
-
-        }
+        public void getResponse(ResultsList resultsList) {}
     };
 }
